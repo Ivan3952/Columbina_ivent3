29b8b26d191fddddb2753b34588aa65f2099789c
Ivan3952 <109902324+Ivan3952@users.noreply.github.com>
2026-05-16 18:09:36 +0300

    Update data.js

--- data.js
+++ data.js
@@ -9,26 +9,4 @@ const EVENT = {
 };
 
 const WORKS = [
-  {
-    id: 1,
-    username: "@example_winner",
-    title: "Пример работы победителя",
-    image: "img/works/placeholder.svg",
-    postLink: "",
-    score: 10,
-    reward: 5000,
-    isWinner: true,
-    place: 1
-  },
-  {
-    id: 2,
-    username: "@example_user",
-    title: "Пример работы участника",
-    image: "img/works/placeholder.svg",
-    postLink: "",
-    score: 8,
-    reward: 1000,
-    isWinner: false,
-    place: null
-  }
 ];
